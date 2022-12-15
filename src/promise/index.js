8241584d3e58775dff675bf6d33c99261edeb475
soadfreyle <soadfre@gmail.com>
2022-12-14 20:56:02 -0500

    promesas

---
+++ src/promise/index.js
@@ -0,0 +1,15 @@
+const cows = 15;
+
+const countCows = new Promise(function (resolve, reject){
+    if (cows > 10) {
+        resolve(`We have ${cows} cows on the farm`);
+    } else {
+        reject("There is no cown on the farm");
+    }
+});
+
+countCows.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+}).finally(() => console.log('Finally'));
